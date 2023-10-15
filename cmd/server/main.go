9c575b09f252e00a97c70c567d81a4ca4a06af01
SavIA <VanyaAS@yandex.ru>
2023-10-15 19:03:29 +0700

    review fix - добавлена поддержка graceful shutdown для сервера

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/WPGe/go-yandex-advanced/internal/handler"
 	"github.com/WPGe/go-yandex-advanced/internal/repository"
 	"github.com/WPGe/go-yandex-advanced/internal/storage"
 	"github.com/go-chi/chi/v5"
-	"log"
+	"golang.org/x/sync/errgroup"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+		<-c
+		cancel()
+	}()
+
 	parseFlags()
 
 	memStorage := storage.NewMemStorage()
@@ -20,5 +35,21 @@ func main() {
 	r.Get("/value/{type}/{name}", handler.MetricGetHandler(repo))
 	r.Get("/", handler.MetricGetAllHandler(repo))
 
-	log.Fatal(http.ListenAndServe(flagRunAddr, r))
+	httpServer := &http.Server{
+		Addr:    flagRunAddr,
+		Handler: r,
+	}
+
+	g, gCtx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		return httpServer.ListenAndServe()
+	})
+	g.Go(func() error {
+		<-gCtx.Done()
+		return httpServer.Shutdown(context.Background())
+	})
+
+	if err := g.Wait(); err != nil {
+		fmt.Printf("exit reason: %s \n", err)
+	}
 }
